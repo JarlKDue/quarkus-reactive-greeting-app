faeb39d59c53eb9f76683152b864224ad422b8b5
JarlKDue <jarldue@localhost.localdomain>
2020-11-04 22:32:20 +0100

    Making it slower

--- src/main/java/org/miracle/dk/test/openshift/app/TestApplication.java
+++ src/main/java/org/miracle/dk/test/openshift/app/TestApplication.java
@@ -7,15 +7,31 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.core.MediaType;
+import java.sql.Timestamp;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class TestApplication {
+
+    private final Logger LOGGER = Logger.getLogger("TestApplication");
 
     @ConfigProperty(name = "greeting", defaultValue = "No One")
     String greeting;
 
     @Route(produces = MediaType.TEXT_PLAIN)
     public Uni<String> hello() {
+        LOGGER.info(new Timestamp(System.currentTimeMillis()).toString());
+        calc();
         return Uni.createFrom().item("Hello " + greeting + " from Quarkus Native");
+    }
+
+    public void calc() {
+            for (int y = 0; y < 50; y++) {
+                for (int i = 0; i < 100000; i++) {
+                    double result = Math.atan(Math.tan(Math.atan(Math.tan(Math.atan(Math.tan(Math.tan(Math.tan(i))))))));
+                }
+            }
+        LOGGER.info(new Timestamp(System.currentTimeMillis()).toString());
+
     }
 }
